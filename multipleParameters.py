d46556029c840e1db25d52635d8aca71dec3dec0
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-30 09:34:56 +0900

    2.7
    MultipleParameters

---
+++ multipleParameters.py
@@ -0,0 +1,8 @@
+def say_hello(user_name, user_age):
+    print("hello", user_name)
+    print("you are", user_age, "years old")
+
+
+say_hello("kamja", 15)
+# "kamja"는 say_hello의 첫번째 parameter인 user_name에 저장
+# 15는 say_hello의 두번째 parameter인 user_age에 저장
