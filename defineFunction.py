c817b1dd37e74a0115df5bf024f365188aa260cb
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-30 09:00:01 +0900

    2.5
    Indentation(들여쓰기)

--- defineFunction.py
+++ defineFunction.py
@@ -6,3 +6,9 @@ def say_hello():
 
 # 함수 사용 즉, 함수를 호출하는 부분
 say_hello()
+
+# def say_bye():
+#   print("bye")
+#   say_hello()
+# say_hello()함수가 들여쓰기 됨으로 say_bye()함수안에서 호출된다.
+# 즉, python은 들여쓰기로 영역을 구분한다. 다른언어에서는 {} 파이썬은 들여쓰기
