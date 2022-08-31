1612ff226951077bfd387842a808fe273fa35d3e
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-31 12:36:33 +0900

    3.1

--- ifElseif.py
+++ ifElseif.py
@@ -3,3 +3,12 @@ if password_correct:
     print("Here is your money")
 else:
     print("Wrong password")
+
+winner = 10
+if winner > 10:
+    print("Winner is greater than 10")
+elif winner < 10:
+    print("Winner is less than 10")
+else:
+    print("Winner is 10")
+# 조건 만족 시 조건문 종료
