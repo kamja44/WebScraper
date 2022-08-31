411128491009f5c1dd554b830f473961d991f00a
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-31 10:36:17 +0900

    2.9
    Default Parameters
    Calculator

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def plus(a=0, b=0):
+    print(a+b)
+
+
+def minus(a=0, b=0):
+    print(a-b)
+
+
+def multiplication(a=1, b=1):
+    print(a*b)
+
+
+def division(a=1, b=1):
+    print(a/b)
+
+
+def square(a=1, b=1):
+    print(a**b)
+
+
+print("plus")
+plus(5, 10)
+plus(5)
+print("minus")
+minus(5, 10)
+minus(5)
+print("multiplication")
+multiplication(5, 10)
+multiplication(5)
+print("division")
+division(5, 10)
+division(5)
+print("square")
+square(5, 10)
+square(5)
---
+++ defaultParameters.py
@@ -0,0 +1,6 @@
+def say_hello(user_name="anonymous"):
+    print("hello", user_name)
+
+
+say_hello("kamja")
+say_hello()
