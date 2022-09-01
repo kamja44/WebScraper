2c1d2da73cfbbfa82cf569a831c55a0a7aa18568
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-01 10:40:33 +0900

    3.5
    While

--- randomFunction.py
+++ randomFunction.py
@@ -1,8 +1,10 @@
+from dis import dis
 import random
-# from random import randint <- random라이브러리(모듈)에서 randint함수를 import한다.
-# import random <- random 라이브러리 통채로 import
-# 외부 라이브러리 사용
-# Python Standard Library <- default function(built-in function)
+"""from random import randint <- random라이브러리(모듈)에서 randint함수를 import한다.
+import random <- random 라이브러리 통채로 import
+외부 라이브러리 사용
+Python Standard Library <- default function(built-in function)
+
 
 user_choice = int(input("Choose number."))
 
@@ -14,3 +16,10 @@ elif user_choice > pc_choice:
     print("Lower! Computer chose", pc_choice)
 elif user_choice < pc_choice:
     print("Higer! Computer chose", pc_choice)
+"""
+distance = 0
+# while True:
+#   print() <- while의 조건이 True일 경우 계속 만복
+while distance < 20:
+    print("I'm running: ", distance, "km")
+    distance = distance + 1
