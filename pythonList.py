a8977325655d98102d57ec1c69d05ddb0d898f98
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-02 14:36:00 +0900

    4.1
    Lists

---
+++ pythonList.py
@@ -0,0 +1,25 @@
+# list
+
+days_of_week = ["Mon", "Tue", "Wed", "Thr", "Fri"]
+
+# list's methods
+
+# print(days_of_week.count("Wed"))
+# days_of_week.clear()
+print(days_of_week)
+# days_of_week.reverse()
+# print(days_of_week)
+days_of_week.append("Sat")
+print(days_of_week)
+days_of_week.append("Sun")
+print(days_of_week)
+days_of_week.remove("Sun")
+print(days_of_week)
+
+# list의 요소에 접근
+# list[index] index는 0부터시작
+print(days_of_week[2])  # Wed출력
+
+# list 안의 list(2중 list)도 가능
+days_of_week.append(["kamja"])
+print(days_of_week)
