349be29bdfff68117b86e718148bb880d6304e04
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-02 14:59:23 +0900

    4.2
    Tuples

--- pythonList.py
+++ pythonList.py
@@ -23,3 +23,8 @@ print(days_of_week[2])  # Wed출력
 # list 안의 list(2중 list)도 가능
 days_of_week.append(["kamja"])
 print(days_of_week)
+
+# list의 뒤에서부터 접근가능
+# 즉, list[-1] = [kamja]출력
+# 뒤에서부터 접근할 때는 -1부터 시작
+print(days_of_week[-1])
