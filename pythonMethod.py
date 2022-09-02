549237d293ea84e88b660fb5b05c916c8299b226
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-02 13:50:04 +0900

    4.0
    Method

---
+++ pythonMethod.py
@@ -0,0 +1,10 @@
+# days_of_week = ["Mon", "Tue", "Wed", "Thr", "Fri"]
+
+# method = 데이터 뒤에 결합(연결)된 function 즉, name 변수에 연결된 upper()함수
+# 변수명.함수명()으로 사용한다.
+name = "kamja"
+print(name.upper())
+print(name.capitalize())
+print(name.startswith("k"))
+print(name.replace("a", "🍙"))
+print(name.endswith("k"))
