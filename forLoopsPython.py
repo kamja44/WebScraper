5fd07137809515d9ffa801548dfeb16a00cf2500
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-05 08:46:14 +0900

    4.5
    For Loops

---
+++ forLoopsPython.py
@@ -0,0 +1,13 @@
+# tuple
+websites = (
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com",
+    "youtube.com",
+)
+
+# websites tuple 안에 있는 각각의 item을 사용해서 코드 실행
+# for 변수명(속성명) in (배열 or 튜플 or list or dictionary)
+for website in websites:
+    print("website is equals to", website)
