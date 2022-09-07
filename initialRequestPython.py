347aabea2d74f0ba370a191e05a56f391abd6be0
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-07 11:19:56 +0900

    5.4
    how to use BeautifulSoup

--- initialRequestPython.py
+++ initialRequestPython.py
@@ -1,4 +1,5 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
@@ -6,4 +7,6 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
-    print(response.text)  # 웹 사이트의 HTML 코드를 볼 수 있다.
+    # print(response.text)  # 웹 사이트의 HTML 코드를 볼 수 있다.
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = print(soup.find_all("section", class_="jobs"))  # class_ == class
