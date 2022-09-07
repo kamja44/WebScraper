6974388f148a8bb4a902b2665859807ee7ecdd59
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-07 11:48:14 +0900

    5,6
    Job Posts

--- initialRequestPython.py
+++ initialRequestPython.py
@@ -10,4 +10,12 @@ else:
     # print(response.text)  # 웹 사이트의 HTML 코드를 볼 수 있다.
     soup = BeautifulSoup(response.text, "html.parser")
     # class_ == class # class는 예약어이기에 class_로 사용
-    jobs = print(soup.find_all("section", class_="jobs"))
+    jobs = soup.find_all("section", class_="jobs")
+    # print(len(jobs))
+    for job_section in jobs:
+        job_posts = job_section.find_all("li")
+        job_posts.pop(-1)  # list에서 마지막 항목 <li class="view-all"> 제거
+        for post in job_posts:
+            print(post)
+            print(
+                "//////////////////////////////////////////////////////////////////////////////")
