39555ca77133c68e2c2a6ac7db024418a145f776
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-07 11:28:28 +0900

    5.5
    position arguments
    Keyword Arguments

--- initialRequestPython.py
+++ initialRequestPython.py
@@ -9,4 +9,15 @@ if response.status_code != 200:
 else:
     # print(response.text)  # 웹 사이트의 HTML 코드를 볼 수 있다.
     soup = BeautifulSoup(response.text, "html.parser")
-    jobs = print(soup.find_all("section", class_="jobs"))  # class_ == class
+    # class_ == class # class는 예약어이기에 class_로 사용
+    jobs = print(soup.find_all("section", class_="jobs"))
+
+
+# def say_hello(name, age):
+#     print(f"Hello {name} you are {age} years old")
+
+
+# position arguments <- argument의 위치를 알고있다. 즉, 자리에 기반하여 argument를 보낸다.
+# say_hello("kamja", 20)
+# keyword arguments <- argumet의 이름에 값을 정해준다. arguemnt의 자리가 바뀌어도 상관없다.
+# say_hello(age=20, name="kamja")
