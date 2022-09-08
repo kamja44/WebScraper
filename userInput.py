7f308179eb9622044fe9b222478ed229b5137485
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-09 00:50:37 +0900

    5.18

---
+++ userInput.py
@@ -0,0 +1,12 @@
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+
+keyword = input("What do you want to Search for?")
+
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
+
+jobs = indeed + wwr # 리스트끼리 더하기
+for job in jobs:
+    print(job)
+    print("/////\n/////")
